1f1f669618cd1747c47b9242a9b550e07f3e33a6
mehmetkicirti <mehmetkicirti@hotmail.com>
2020-07-18 17:41:30 +0300

    have been created to send props (IEpisodeProps).

--- src/utils/serviceInterfaces.ts
+++ src/utils/serviceInterfaces.ts
@@ -1,3 +1,10 @@
+import { IEpisode } from "../models/IEpisode";
+
+/**
+ * 
+ * All we needed Interfaces defined in here
+ */
+
 export interface IState {
     episodes : Array<any>;
     favourites: Array<any>;
@@ -12,4 +19,10 @@ export enum ActionTypes{
     FETCH_DATA="FETCH_DATA",
     ADD_FAV="ADD_FAV",
     REMOVE_FAV="REMOVE_FAV"
+}
+
+export interface IEpisodeProps {
+    episodes : Array<IEpisode>,
+    toggleFavAction : (episode:IEpisode) => {},
+    favourites : Array<IEpisode>
 }
